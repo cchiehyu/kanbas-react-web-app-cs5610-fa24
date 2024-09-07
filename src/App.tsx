3338948d92d7d7cb93f4180826e5eaada0088a3b
Chieh-Yu <ccychen915@gmail.com>
2024-09-07 16:43:04 -0400

    adding the paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Labs from "./Labs";
+import Lab1 from './Labs/Lab1';
 
 export default function App() {
   return (
     <div>
+      {/* Main heading and subheading */}
       <h1>Welcome to CS5610 Fall - Kanbas Web App Dev</h1>
       <h2>Chieh-Yu (Joyce) Chen</h2>
+
+      {/* Navigation Links */}
       <ul>
         <li><a href="#Labs">Labs</a></li>
         <ul>
@@ -17,6 +21,24 @@ export default function App() {
         <li><a href="#Kanbas">Kanbas</a></li>
         <li><a href="#Github">GitHub</a></li>
       </ul>
+
+      {/* Lab Components */}
+      <div id="labs">
+        <Lab1 />
+      </div>
+      
+      {/* Placeholder for other sections */}
+      <div id="kanbas">
+        <h3>Kanbas</h3>
+        <p>This is the Kanbas section.</p>
+      </div>
+
+      <div id="github">
+        <h3>GitHub</h3>
+        <p>This is the GitHub section.</p>
+      </div>
     </div>
+
+
   );
 }
