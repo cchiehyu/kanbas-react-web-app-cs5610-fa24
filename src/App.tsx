918f539d08d3d67913cb0f3ab7e27b5fa144b6fe
Chieh-Yu <ccychen915@gmail.com>
2024-09-07 16:23:10 -0400

    make the title responsive url

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import Labs from "./Labs";
 export default function App() {
   return (
     <div>
-      <h1>Welcome to Kanbas - by Chieh-Yu Chen</h1>
-      <Labs />
+      <h1>Welcome to CS5610 Fall - Kanbas Web App Dev</h1>
+      <h2>Chieh-Yu (Joyce) Chen</h2>
+      <ul>
+        <li><a href="#Labs">Labs</a></li>
+        <ul>
+          <li><a href="#lab1">Lab 1</a></li>
+          <li><a href="#lab2">Lab 2</a></li>
+          <li><a href="#lab3">Lab 3</a></li>
+        </ul>
+        <li><a href="#Kanbas">Kanbas</a></li>
+        <li><a href="#Github">GitHub</a></li>
+      </ul>
     </div>
   );
 }
