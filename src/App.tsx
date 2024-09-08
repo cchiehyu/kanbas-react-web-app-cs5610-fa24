4a33978f1e7576a1c7d0f7167ea4b4f78c943796
Chieh-Yu <ccychen915@gmail.com>
2024-09-08 14:29:06 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ export default function App() {
       <img id="wd-starship"
         width="400px"
       src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-      />
+      alt=""/>
       <br />
       Loading a local image:
       <br />
