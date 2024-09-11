c17417d78f7d1595aba4d2c4093982b7f751bfc5
Chieh-Yu <ccychen915@gmail.com>
2024-09-10 20:44:38 -0400

    A1 work in progress draft 1

--- src/App.tsx
+++ src/App.tsx
@@ -11,35 +11,6 @@ export default function App() {
         <h1>Welcome to CS5610 Fall - Kanbas Web App Dev</h1>
         <h2>Chieh-Yu (Joyce) Chen</h2>
 
-        {/* Navigation Links */}
-        {/* <ul>
-          <li><a href="#Labs">Labs</a></li>
-          <ul>
-            <li><a href="#lab1">Lab 1</a></li>
-            <li><a href="#lab2">Lab 2</a></li>
-            <li><a href="#lab3">Lab 3</a></li>
-          </ul>
-
-          <li>
-            <a 
-              href="https://www.linkedin.com/in/joyce-chen-1686b3199/" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              LinkedIn -- Click Me!
-            </a>
-          </li>
-          <li>
-            <a 
-              href="https://github.com/cchiehyu/kanbas-react-web-app-cs5610-fa24" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              GitHub -- Click Me!
-            </a>
-          </li>
-        </ul> */}
-
           {/* Lab Components */}
           <HashRouter>
           <div>
@@ -56,11 +27,10 @@ export default function App() {
         {/* Placeholder for other sections */}
 
 
-      <h4>Anchor tag</h4>
-      <a id="wd-linkedin"  href="https://www.linkedin.com/in/joyce-chen-1686b3199/"> Linkedin </a>
-      to my Linkedin<br/>
-      <a id="wd-github"  href="https://github.com/cchiehyu/kanbas-react-web-app-cs5610-fa24"> Github </a>
-      to my Github<br/>
+        <h4>Anchor tag</h4>
+        Please
+        <a id="wd-lipsum" href="https://www.lipsum.com"> click here </a>
+        to get dummy text<br/>
       </div>
   );
 }
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,10 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link id="wd-linkedin" to="https://www.linkedin.com/in/joyce-chen-1686b3199/">Linkedin</Link></li>
+        <li><Link id="wd-github" to="https://github.com/cchiehyu/kanbas-react-web-app-cs5610-fa24">Github</Link></li>
       </ul>
+      
     );
   }
   
