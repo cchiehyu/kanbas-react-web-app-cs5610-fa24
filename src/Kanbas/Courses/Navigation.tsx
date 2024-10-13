6a952ccfc5d1e876ac39f14aae2bfcfe4579c7dc
Chieh-Yu <ccychen915@gmail.com>
2024-10-13 16:49:58 -0400

    3.5 Data Driven Course Navigation

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useParams, useLocation } from 'react-router-dom';
+import '../styles.css'; // Correct path to the styles.css file
+
 export default function CoursesNavigation() {
+  // Get the course ID from the URL parameters
+  const { cid } = useParams();
+  
+  // Get the current location (pathname)
+  const location = useLocation();
+
+  // Array of navigation links
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+
   return (
-    <div id="wd-courses-navigation"className="wd list-group fs-5 rounded-0">
-      <Link id="wd-course-home-link" className="list-group-item active border border-0"   to="/Kanbas/Courses/1234/Home">Home</Link>
-      <Link id="wd-course-modules-link" className="list-group-item active border border-0" to="/Kanbas/Courses/1234/Modules">Modules
-        </Link><br/>
-      <Link id="wd-course-piazza-link" className="list-group-item active border border-0"  to="/Kanbas/Courses/1234/Piazza">Piazza</Link>
-      <Link id="wd-course-zoom-link" className="list-group-item active border border-0"    to="/Kanbas/Courses/1234/Zoom">Zoom</Link>
-      <Link id="wd-course-quizzes-link" className="list-group-item active border border-0" to="/Kanbas/Courses/1234/Assignments">
-          Assignments</Link><br/>
-      <Link id="wd-course-assignments-link" className="list-group-item active border border-0" to="/Kanbas/Courses/1234/Quizzes">Quizzes
-        </Link><br/>
-      <Link id="wd-course-grades-link" className="list-group-item active border border-0" to="/Kanbas/Courses/1234/Grades">Grades</Link>
-      <Link id="wd-course-people-link" className="list-group-item active border border-0"  to="/Kanbas/Courses/1234/People">People</Link>
+    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => {
+        const linkPath = `/Kanbas/Courses/${cid}/${link}`;
+        const isActive = location.pathname.includes(link.toLowerCase());
+
+        return (
+          <Link
+            key={link}
+            id={`wd-course-${link.toLowerCase()}-link`}
+            className={`list-group-item ${isActive ? 'active' : ''}`}
+            to={linkPath}
+          >
+            {link}
+          </Link>
+        );
+      })}
     </div>
-);}
+  );
+}
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import { courses } from '../Database';  
 import CoursesNavigation from "./Navigation";
 import Home from "./Home";
 import Modules from "./Modules";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from './People/Table';
+import { FaAlignJustify } from 'react-icons/fa';
 
 export default function Courses() {
   const { cid: courseCode } = useParams<{ cid: string }>();
+  const course = courses.find((course) => course._id === courseCode);
 
   return (
     <div id="wd-courses">
-      <h2 className="text-danger">Course {courseCode}</h2> 
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        {course && course.name}
+      </h2>
       <hr />
 
       <div className="d-flex">
