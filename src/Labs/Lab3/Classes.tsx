550c075d3c12d314c9bbf8053d1def783f1c235e
Chieh-Yu <ccychen915@gmail.com>
2024-11-10 15:18:38 -0500

    Update Classes.tsx

--- src/Labs/Lab3/Classes.tsx
+++ src/Labs/Lab3/Classes.tsx
@@ -1,15 +1,17 @@
 import './Classes.css';
-import React from 'react';
-
 export default function Classes() {
+  const color = 'blue';
   const dangerous = true;
-
   return (
-    <div id="wd-classes">
+    <div>
       <h2>Classes</h2>
-      <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'} wd-fg-black wd-padding-10px`}>
-        Dangerous background
+      <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
+        Yellow background  </div>
+        <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+        Dynamic Blue background
       </div>
-    </div>
-  );
-}
+      <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'}
+                                     wd-fg-black wd-padding-10px`}>
+       Dangerous background
+     </div>
+    </div> ) };
