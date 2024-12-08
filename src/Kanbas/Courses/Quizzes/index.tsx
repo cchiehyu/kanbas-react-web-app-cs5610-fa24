fd7c13285ff91f5b73528952683e469a0ba4426a
Chieh-Yu <ccychen915@gmail.com>
2024-12-08 17:10:34 -0500

    Update index.tsx

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -218,15 +218,17 @@ export default function QuizList() {
                     <span>|</span>
                     <span>Due {formatDate(quiz.dueDate)}</span>
                     <span>|</span>
-                    {currentUser.role === 'STUDENT' ? (
-                      <span>
-                        Score: {getStudentScore(quiz._id) !== undefined 
-                          ? `${getStudentScore(quiz._id)}/${quiz.points}` 
-                          : 'Not submitted'}
-                      </span>
-                    ) : (
-                      <span>{quiz.points} pts</span>
-                    )}
+                    <span>Points {quiz.points}</span>
+                    {currentUser.role === 'STUDENT' && (
+                      <>
+                        <span>|</span>
+                        <span>
+                          Score: {getStudentScore(quiz._id) !== undefined 
+                            ? `${getStudentScore(quiz._id)}/${(quiz.points)}` 
+                            : 'Not submitted'}
+                        </span>
+                      </>
+                      )}
                     <span>|</span>
                     <span>{quiz.numberOfQuestions} Questions</span>
                   </div>
